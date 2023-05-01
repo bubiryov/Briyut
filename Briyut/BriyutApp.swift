72b3a0a2c724d5ea58818dd1f71944ce34de1624
Egor <bubiryov@icloud.com>
2023-05-01 17:54:23 +0300

    Initial Commit

---
+++ Briyut/BriyutApp.swift
@@ -0,0 +1,17 @@
+//
+//  BriyutApp.swift
+//  Briyut
+//
+//  Created by Egor Bubiryov on 01.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct BriyutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
